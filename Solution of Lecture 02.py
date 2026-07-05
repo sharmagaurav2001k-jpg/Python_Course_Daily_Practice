6b2c9c9c59d0edb51ff865cbc715adb9d5e8b571
Gaurav Sharma <sharmagaurav2001k@mail.com>
2026-07-05 09:23:38 +0530

    Initial commit

---
+++ Solution of Lecture 02.py
@@ -0,0 +1,40 @@
+# Task: Given word ko output me show karna hai.
+
+'''
+Demo Task: Good Morning
+
+Solution:
+
+print("Good Morning")
+
+Output:
+
+ Good Morning
+ 
+ '''
+
+
+
+# Task 1: Welcome to Python
+
+print("Welcome to python")
+
+# Task 2: My name is Rohan
+
+print("My name is Rohan")
+
+# Task 3: I am learning Python programming
+
+print("I am learning Python programming")
+ 
+# Task 4: Thank You!
+
+print("Thank You!")
+
+
+
+'''                               End of the Solution of Lecture 02.py                     '''
+
+
+a = "Mera Name Rohan hai""Karan"
+print(a)
---
+++ uniqe.py
@@ -0,0 +1,12 @@
+'''
+
+fruit = "Orange"
+weight = 1.5 "kg"
+
+
+print(fruit)
+print(weight)
+
+'''
+
+print("#Grv")
